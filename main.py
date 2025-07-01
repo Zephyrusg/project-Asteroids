26792f3002be3b15548e969d6be4f5ea2d171a03
zephyrusg <bas.dekoning@gmail.com>
2025-07-01 09:07:07 +0200

    visual working

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            screen.fill("black")
+            pygame.display.flip()
 
 
 if __name__ == "__main__":
